0aabd5c2181ef8d5dc7d70021902ca5f0ce85c20
celso.rivera@ecoambiental.com.br <celso.rivera@ecoambiental.com.br>
2021-12-02 12:58:17 -0300

    Add project files.

---
+++ AvaliacaoTecnica1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AvaliacaoTecnica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int div;
+            int result = 9;
+
+            do
+            {
+                div = 0;
+                result++;
+                for (i = 1; i <= 10; i++)
+                {
+                    if (result % i == 0)
+                    {
+                        div++;
+
+                    }
+                }
+
+            }
+            while (div != 10);
+
+            Console.WriteLine("Menor número divisível por 1 a 20 = " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
